5f680939ed30854e0eb2bf44c7b78fa4daab21c0
jesse <jnguy887@ucr.edu>
2024-04-27 12:24:54 -0700

    Added assets + Basic Title Screen

--- App.js
+++ App.js
@@ -1,14 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image} from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import Button from './components/buttonTitleStart';
+
+const titleImage = require('./assets/Cat_Base_Sprite.png');
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello There</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <View style={styles.container}>
+        <Image source={titleImage} style={styles.image} />
+        <Text>Hello There</Text>
+        <StatusBar style="auto" />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button label="Start" />
+      </View>
+    </SafeAreaProvider>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -17,4 +29,20 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  footerContainer: {
+    flex: 1/10,
+    alignItems: 'center',
+    backgroundColor: '#0FF',
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+  },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
+
 });
